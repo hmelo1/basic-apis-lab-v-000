93887eedb69ab7b51a7b89617b1f10d3da005fed
Eddie Melo <heriberto@melo.nyc>
2018-05-15 16:51:32 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,6 +4,20 @@ class RepositoriesController < ApplicationController
 
   end
 
-  def github_search
-  end
+  def foursquare
+   @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+     req.params['client_id'] = 'O2S1JF343O15KATMNLLWCOIMVDVPUI4TATZYNKHUF22AKUND'
+     req.params['client_secret'] = 'G33CJ1Q2AGVJ4GWLDYUGBMXENJSWYC1MFOA25IWBTJNR3YU0'
+     req.params['v'] = '20160201'
+     req.params['near'] = params[:zipcode]
+     req.params['query'] = 'coffee shop'
+   end
+   body_hash = JSON.parse(@resp.body)
+   if @resp.success?
+    @venues = body["response"]["venues"]
+   else
+    @error = body["meta"]["errorDetail"]
+   end
+   render 'search'
+ end
 end
