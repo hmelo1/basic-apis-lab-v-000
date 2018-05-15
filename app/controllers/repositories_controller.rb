e07daf7ce9c221c47446e35bc40e67e15f6572e0
Eddie Melo <heriberto@melo.nyc>
2018-05-15 16:53:14 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,13 +8,11 @@ class RepositoriesController < ApplicationController
    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
      req.params['client_id'] = 'Iv1.cde26eac6e7e1da0'
      req.params['client_secret'] = '69b02369b74e206e2a4bfecc25117998e361a81e'
-     req.params['v'] = '20160201'
-     req.params['near'] = params[:zipcode]
-     req.params['query'] = 'coffee shop'
+     req.params['q'] = params[:query]
    end
    body_hash = JSON.parse(@resp.body)
    if @resp.success?
-    @venues = body["response"]["venues"]
+    @venues = body['items']
    else
     @error = body["meta"]["errorDetail"]
    end
